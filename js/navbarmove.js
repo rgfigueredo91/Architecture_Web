2cd9fec427405ec92380c4ffa63ff9b031f2842f
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2022-04-12 22:20:18 -0300

    j

---
+++ js/navbarmove.js
@@ -0,0 +1,24 @@
+let lastScrollTop = 100;
+			navbar = document.getElementById("navbar-noboots");
+			contact = document.getElementById("title-contact");
+			menu = document.getElementById("title-menu");
+			window.addEventListener("scroll", function () {
+				var scrollTop = window.pageYOffset || document
+					.documentElement.scrollTop;
+				if (scrollTop < 1) {
+					navbar.style.background = "none";
+					navbar.style.color = "white";
+					contact.style.color = "white";
+					menu.style.color = "white";
+					
+					
+				} else {
+					navbar.style.background = "white";
+					navbar.style.color = "black";
+					contact.style.color = "black";
+					menu.style.color = "black";
+					
+					
+				}
+				lastScrollTop = scrollTop;
+			})
